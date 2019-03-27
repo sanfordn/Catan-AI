1adb11a67e0f2fd70a22083b33e04c3a00b1b0ef
sanfordn <sanfordn@uni.edu>
2019-03-27 10:01:28 -0500

    Emptying rando

--- rando.py
+++ rando.py
@@ -1,9 +1,1 @@
-import random
 
-def think():
-    if command == "setup":
-        return random.randint(0,53)
-    if command == "build":
-        return "-b"
-    elif command == "end":
-        return "-e"
